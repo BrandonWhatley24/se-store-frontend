a700736b8e49f864b57b7165fe9f633e454a3ec5
Brandon Whatley <bwhatl00@g.uafs.edu>
2023-09-18 20:06:07 -0500

    Finshed the design of all three of the pages

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,54 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <LoginPage/>
+      <div v-show="showLoginPage">
+        <LoginPage />
+      </div>
+      <div v-show="showMainPage">
+        <MainPage />
+      </div>
+      <div v-show="showTransactions">
+        <Transactions />
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import LoginPage from './components/LoginPage'
+import LoginPage from './pages/LoginPage'
+import MainPage from './pages/MainPage'
+import Transactions from './pages/Transactions'
 
 export default {
   name: 'App',
 
   components: {
     LoginPage,
+    MainPage,
+    Transactions
   },
 
   data: () => ({
-    //
+    showLoginPage: false,
+    showMainPage: true,
+    showTransactions: false,
   }),
 };
 </script>
